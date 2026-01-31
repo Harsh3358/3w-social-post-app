353ececdec6514d227024ed2a679ebf5a137084e
Harsh3358 <harshkanti1234@gmail.com>
2026-02-01 03:03:58 +0530

    Setup backend skeleton with Express and MongoDB

---
+++ backend/src/routes/health.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Backend is running",
+  });
+});
+
+export default router;
